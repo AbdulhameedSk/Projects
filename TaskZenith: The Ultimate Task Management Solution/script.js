67109af79dac5c5a82fa847ffa4acd63178ff0b6
SHAIK ABDULHAMEED <99593105+AbdulhameedSk@users.noreply.github.com>
2023-09-21 11:01:33 +0530

    Add files via upload

---
+++ TaskZenith: The Ultimate Task Management Solution/script.js
@@ -0,0 +1,59 @@
+// Get references to HTML elements
+const taskInput = document.getElementById("task");
+const dueDateInput = document.getElementById("due-date");
+const tasksContainer = document.getElementById("tasks");
+
+// Initialize tasks array
+let tasks = [];
+
+// Function to add a task
+function addTask() {
+    const taskText = taskInput.value.trim();
+    const dueDate = dueDateInput.value;
+
+    if (taskText === "") {
+        alert("Please enter a task.");
+        return;
+    }
+
+    // Create a task object
+    const task = {
+        text: taskText,
+        dueDate: dueDate
+    };
+
+    // Add the task to the tasks array
+    tasks.push(task);
+
+    // Clear the input fields
+    taskInput.value = "";
+    dueDateInput.value = "";
+
+    // Render the tasks
+    renderTasks();
+}
+
+// Function to render tasks
+function renderTasks() {
+    tasksContainer.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+        const taskDiv = document.createElement("div");
+        taskDiv.className = "task";
+        taskDiv.innerHTML = `
+            <p>${task.text}</p>
+            <p>Due Date: ${task.dueDate}</p>
+            <button onclick="deleteTask(${index})">Delete</button>
+        `;
+        tasksContainer.appendChild(taskDiv);
+    });
+}
+
+// Function to delete a task
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    renderTasks();
+}
+
+// Initial rendering
+renderTasks();
